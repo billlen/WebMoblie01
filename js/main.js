6217c32972fccf70a34519f6ca5983f39b9ae621
wuxueqing <wuxueqing@hihooray.com>
2017-02-07 18:43:45 +0800

    更新

--- js/main.js
+++ js/main.js
@@ -5,27 +5,24 @@
 /**
  * 当文档加载完成才会执行
  */
-$(function(){
-
-
-    $(window).on('resize',resize).trigger('resize');
-    function resize(){
+$(function () {
+    $(window).on('resize', resize).trigger('resize');
+    //根据屏幕宽度的变化而决定轮播图应该展示什么
+    function resize() {
         var windowWidth = $(window).width();
-
-        console.log("111");
-        console.log(windowWidth);
-        var isSmallWidth = windowWidth<768;
+        var isSmallWidth = windowWidth < 768;
         //trigger开始时被调用一下;
-
-        $("#main_ad>.carousel-inner>.item").each(function(i,item){
-            console.log("222");
-            //拿到的是dom对象,转换成jquery对象;
+        $("#main_ad>.carousel-inner>.item").each(function(i,item) {
+            //拿到的是dom对象,转换成jquery对象;才能使用jQuery方式操作;
             var $item = $(item);
-            console.log("333");
-            console.log(isSmallWidth);
-            console.log($item.data(isSmallWidth?"image-xs":"image-lg"));
-            $item.css("backgroundImage",'url("'+$item.data(isSmallWidth?"image-xs":"image-lg")+'")');
-            console.log("444");
+            //data("abc")用于取该元素上面的属性值(data-abc);
+            var imgSrc = isSmallWidth ? $item.data("image-xs") : $item.data("image-lg");
+            $item.css("backgroundImage", 'url("' + imgSrc + '")');
+            if (isSmallWidth) {
+                $item.html('<img src="' + imgSrc + '" alt="" />');
+            }else{
+                $item.html("");
+            }
         })
     }
 })
